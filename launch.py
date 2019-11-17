29cfb3fba8c1d6059a03205afbf5fcdeeabc96c2
Zak (Desktop) <zakcatherall@outlook.com>
2019-11-17 23:15:41 +0000

    Final draft WIP

---
+++ launch.py
@@ -0,0 +1,14 @@
+# import subprocess
+# test = subprocess.Popen(["cd","dm-env"], stdout=subprocess.PIPE)
+# output = test.communicate()[0]
+
+import os
+
+os.chdir("C:\Users\Zak\Desktop\data-mining\launch.py")
+os.system("cd dm-env")
+os.system("cd Scripts")
+os.system("activate")
+os.system("cd")
+os.system("cd")
+os.system("jupyter notebook")
+
